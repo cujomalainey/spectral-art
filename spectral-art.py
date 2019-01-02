749a8feb5307a1be908b5e6fb79e90b27c97975b
Curtis Malainey <curtis@malainey.com>
2019-01-01 20:22:09 -0800

    missed character

--- spectral-art.py
+++ spectral-art.py
@@ -22,7 +22,7 @@ parser.add_argument('--width',  '-x', default=200, type=int,
 parser.add_argument('--frequency-count', '-f', default=-1, type=int,
                     help='force a frequency count and linear interpolate the image')
 parser.add_argument('--upper-frequency', '-u', default=20000, type=int,
-                    help='upper frequency threshold for image (default 20000Hz)')
+                    help='upper frequency threshold for image (default: 20000Hz)')
 parser.add_argument('--lower-frequency', '-l', default=20, type=int,
                     help='lower frequency threshold for image (default: 20Hz)')
 parser.add_argument('--geometric-signal-decay', '-g', default=0, type=verifyDecay,

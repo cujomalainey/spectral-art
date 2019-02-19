2a675033b41c929a9329cb7dd62280bc718d5413
Curtis Malainey <curtis@malainey.com>
2019-02-18 22:03:41 -0800

    fiddling

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use wavefile::WaveFile;
 
 fn main() {
     println!("Hello, world!");
-    let f = WaveFile::open("./fixtures/test-s24le.wav").unwrap();
+    let f = WaveFile::open("holdon.wav").unwrap();
     println!("{:}", f.sample_rate());
+    let mut iter = f.iter();
+    let frame = iter.nth(0).unwrap();
 }

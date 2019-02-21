f7388026857524ebded9f8c13b80d8528c172d17
Curtis Malainey <curtis@malainey.com>
2019-02-20 20:38:28 -0800

    this is hard

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+extern crate num;
+
 use wavefile::WaveFile;
+
+fn avg(v: &Vec<i32>) -> i32 {
+    let x: i32 = v.iter().sum();
+    let y: i32 = v.len() as i32;
+    x / y
+}
 
 fn main() {
     println!("Hello, world!");
@@ -6,5 +14,6 @@ fn main() {
     println!("{:}", f.sample_rate());
     let mut iter = f.iter();
     let frame = iter.nth(0).unwrap();
-    println!("Data is {:?}", frame);
+
+    println!("Data is {:?}", avg(&frame));
 }

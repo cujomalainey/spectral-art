3baf061cf36f3377ae2efb11a16a1353e3bdb2fa
Curtis Malainey <curtis@malainey.com>
2019-03-06 23:06:04 -0800

    Add start of window function lookup system

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use image::{ImageBuffer, Rgb};
 use rustfft::FFTplanner;
 use rustfft::num_complex::Complex;
 use rustfft::num_traits::Zero;
+use rustfft::num_traits::One;
 use std::env;
 use std::collections::HashMap;
 use wavefile::WaveFile;
@@ -35,6 +36,8 @@ const SCALING_AMPLITUDE: &str = "amplitude";
 const SCALING_SMOOTHING_FACTOR: &str = "smoothing_factor";
 const SCALING_LOWER_FREQUENCY: &str = "lower_frequency";
 const SCALING_UPPER_FREQUENCY: &str = "upper_frequency";
+
+type WindowFunctionType = fn(usize) -> Vec<f32>;
 
 fn avg(v: &Vec<i32>) -> i32 {
     let x: i32 = v.iter().sum();
@@ -85,12 +88,30 @@ fn create_image(image_section: &HashMap<String, String>) -> ImageBuffer<Rgb<u8>,
     imgbuf
 }
 
+// TODO implement long list of functions
+fn window_rectangle(n: usize) -> Vec<f32> {
+    vec![One::one(); n]
+}
+
+// TODO implement matching to functions
+fn get_window_function(fft_section: &HashMap<String, String>) -> WindowFunctionType {
+    let window_function = fft_section.get(FFT_WINDOW_FUNCTION).unwrap();
+    window_rectangle
+}
+
+fn get_fft_window(fft_section: &HashMap<String, String>) -> Vec<f32> {
+    let fft_width: usize = fft_section.get(FFT_WIDTH).unwrap().parse().unwrap();
+    get_window_function(fft_section)(fft_width)
+}
+
 fn main() {
     let args: Vec<_> = env::args().collect();
     let conf = Ini::load_from_file(&args[1]).unwrap();
 
     let audio_section = conf.section(Some(SECTION_MUSIC)).unwrap();
     let image_section = conf.section(Some(SECTION_IMAGE)).unwrap();
+    let fft_section = conf.section(Some(SECTION_FFT)).unwrap();
+    let fft_decimations: u8 = fft_section.get(FFT_DECIMATIONS).unwrap().parse().unwrap();
     // let channels:   u8 = audio_section.get(MUSIC_CHANNEL).unwrap().parse().unwrap();
     // let is_mono:    bool = audio_section.get(MUSIC_IS_MONO).unwrap().parse().unwrap();
     // let start_time: f32 = audio_section.get(MUSIC_START_TIME).unwrap().parse().unwrap();
@@ -99,29 +120,24 @@ fn main() {
     let f = load_audio_file(audio_section);
     let img = create_image(image_section);
     let gradient = load_gradient_file(image_section);
-
-    let fft_section = conf.section(Some(SECTION_FFT)).unwrap();
-    let fft_width: usize = fft_section.get(FFT_WIDTH).unwrap().parse().unwrap();
-    let fft_decimations: u8 = fft_section.get(FFT_DECIMATIONS).unwrap().parse().unwrap();
-    let window_function = fft_section.get(FFT_WINDOW_FUNCTION).unwrap();
-
+    let window = get_fft_window(fft_section);
 
     let mut iter = f.iter();
 
-    let mut input:  Vec<Complex<f32>> = vec![Zero::zero(); 0];
-    let mut output: Vec<Complex<f32>> = vec![Zero::zero(); fft_width];
+    // let mut input:  Vec<Complex<f32>> = vec![Zero::zero(); 0];
+    // let mut output: Vec<Complex<f32>> = vec![Zero::zero(); fft_width];
 
-    for _i in 0..fft_width as i32 {
-        let frame = iter.nth(0).unwrap();
-        input.push(Complex::new(avg(&frame) as f32, 0.0));
-    }
-
-    let mut planner = FFTplanner::new(false);
-    let fft = planner.plan_fft(fft_width);
-    fft.process(&mut input, &mut output);
-
-    // for i in 0..513 {
-    for i in 0..4 {
-        println!("{:?}", output[i]);
-    }
+    // for _i in 0..fft_width as i32 {
+    //     let frame = iter.nth(0).unwrap();
+    //     input.push(Complex::new(avg(&frame) as f32, 0.0));
+    // }
+    //
+    // let mut planner = FFTplanner::new(false);
+    // let fft = planner.plan_fft(fft_width);
+    // fft.process(&mut input, &mut output);
+    //
+    // // for i in 0..513 {
+    // for i in 0..4 {
+    //     println!("{:?}", output[i]);
+    // }
 }
